d9963e3ff33c52fa7241ee33d0dfb51b1cd678a5
victor-estrada <victorestrada.ux@gmail.com>
2017-09-11 19:09:54 -0500

    added nav links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#home"
+  get "about" => "pages#about"
+  get "projects" => "pages#projects"
+  get "contact" => "pages#contact"
 end
